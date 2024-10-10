13102976204ce561ac932cb14aee67462ae8269c
0xd34df00d <0xd34df00d@gmail.com>
2024-10-10 11:20:52 -0500

    Move the stack definition out to a separate module

--- app/Main.hs
+++ app/Main.hs
@@ -11,30 +11,7 @@ import Data.Vector.Unboxed.Mutable qualified as VM
 import Data.Word
 
 import Data.Ch
-
-data Stack s a = Stack
-  { theVec :: VM.MVector s a
-  , size :: Int
-  }
-
-mkStack :: VM.Unbox a => Int -> ST s (Stack s a)
-mkStack initSize = (`Stack` 0) <$> VM.unsafeNew initSize
-
-isEmpty :: Stack s a -> Bool
-isEmpty Stack{..} = size == 0
-
-push :: VM.Unbox a => a -> Stack s a -> ST s (Stack s a)
-push a Stack{..} = do
-  vec' <- if size /= VM.length theVec
-             then pure theVec
-             else theVec `VM.unsafeGrow` size
-  VM.unsafeWrite vec' size a
-  pure $ Stack vec' (size + 1)
-
-pop :: VM.Unbox a => Stack s a -> ST s (a, Stack s a)
-pop Stack{..} = do
-  a <- VM.unsafeRead theVec (size - 1)
-  pure (a, Stack theVec (size - 1))
+import Data.Stack
 
 isOpen :: Word8 -> Bool
 isOpen [c|(|] = True
